e8ff2636753ef2bbf9ef6a8929f12ce1ef8496ef
Joseph Taiwo <104542152+Teejay128@users.noreply.github.com>
2023-09-07 23:45:39 +0100

    resolved most error messages from tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,68 +1,77 @@
-jest.useFakeTimers()
-
+jest.useFakeTimers();
 
 const ShortUrl = require("../app/models/shortUrlModel");
 const app = require("../app/index");
 const sum = require("../sum");
+const mongoose = require("mongoose");
 const db = require("../utils/database");
 
 const testURL = "mongodb://localhost:27017/linkScissor_test";
 
 // Scissor router API tests
 describe("tests for scissor functionality", () => {
-  beforeAll(() => {
-    db.connect(testURL);
+    beforeAll(async () => {
+        await db.connect(testURL)
 
-    ShortUrl.insertMany([
-      {
-        longUrl: "https://www.mongodb.com/docs/mongodb-shell/crud/insert/",
-        shortUrl: "localhost:4000/mongo",
-        urlCode: "mongo",
-        qrCode: "This is a long qrcode whatever",
-      },
-      {
-        longUrl: "https://mongoosejs.com/docs/models.html",
-        shortUrl: "localhost:4000/goose",
-        urlCode: "goose",
-        qrCode: "This is a long qrcode whatever",
-      },
-    ]);
-  });
+        ShortUrl.insertMany([
+            {
+                longUrl:
+                    "https://www.mongodb.com/docs/mongodb-shell/crud/insert/",
+                shortUrl: "localhost:4000/mongo",
+                urlCode: "mongo",
+                qrCode: "This is a long qrcode whatever",
+            },
+            {
+                longUrl: "https://mongoosejs.com/docs/models.html",
+                shortUrl: "localhost:4000/goose",
+                urlCode: "goose",
+                qrCode: "This is a long qrcode whatever",
+            },
+        ]);
 
-  describe("learning how to test", () => {
-    test("adds 1 + 2 to equal 3", () => {
-      expect(sum(1, 2)).toBe(3);
+        // done();
     });
-  });
 
-  describe("requesting for all available scissors", () => {
-    // All the added shortUrls should reflect in the database
-    // The number returned must be equal to the number of links
-    // Should also test for like 2 of the available links
-  });
+    // beforeAll((done) => {
+    //   done()
+    // })
 
-  describe("creating a new scissor", () => {
-    // Random url code if customCode is not provided
-    // A test block for qrCode and its details
-    // Return error if urlCode already exists
-    // Create newScissor with the right details
-  });
+    describe("learning how to test", () => {
+        test("adds 1 + 2 to equal 3", () => {
+            expect(sum(1, 2)).toBe(3);
+        });
+    });
 
-  describe("clicking on a scissor", () => {
-    // number of clicks should increase by 1
-  });
+    // test("requesting for all available scissors", () => {
+    //   // All the added shortUrls should reflect in the database
+    //   // The number returned must be equal to the number of links
+    //   // Should also test for like 2 of the available links
+    // });
 
-  describe("deleting a scissor", () => {
-    // The particular link should not be found in the database
-  });
+    // test("creating a new scissor", () => {
+    //   // Random url code if customCode is not provided
+    //   // A test block for qrCode and its details
+    //   // Return error if urlCode already exists
+    //   // Create newScissor with the right details
+    // });
 
-  afterAll(() => {
-    // Clear the test database
-    ShortUrl.find({})
-      .then((res) => console.log(res))
-      .then(() => ShortUrl.deleteMany())
-      .then((count) => console.log(count))
-      .then(() => db.disconnect())
-      .catch((err) => console.log(err));
-  });
+    // test("clicking on a scissor", () => {
+    //   // number of clicks should increase by 1
+    // });
+
+    // test("deleting a scissor", () => {
+    //   // The particular link should not be found in the database
+    // });
+
+    afterAll((done) => {
+        // Clear the test database
+        ShortUrl.find({})
+            .then((res) => console.log(res))
+            .then(() => ShortUrl.deleteMany())
+            .then((count) => console.log(count))
+            .then(() => db.disconnect())
+            .catch((err) => console.log(err));
+
+        done();
+    });
 });
